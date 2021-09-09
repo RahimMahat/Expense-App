d1684bccdf336370c5f8de3b694f990b5ae95888
RahimMahat <rahimmahat99@gmail.com>
2021-09-09 17:37:37 +0530

    dynamically adding new data in the expenses list

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -17,26 +17,15 @@ function Expenses(props) {
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      />
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      />
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      />
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      />
+      {/* Taking the expenses array from App.js then by using map rendering the ExpenseItem element as jsx allows us to do that  */}
+      {props.items.map((expense) => (
+        <ExpenseItem
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+      {/* removed the hardcoded Expenseitem elements to display the data dynamically  */}
     </Card>
   );
 }
